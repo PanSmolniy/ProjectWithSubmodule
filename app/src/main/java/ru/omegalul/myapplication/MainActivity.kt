8c8fde2af549353da8ddd65dedb4233433f0d785
dmitriy.smolyaninov <dmitriy.smolyaninov@doconcall.ru>
2018-08-08 17:46:49 +0300

    Test

--- app/src/main/java/ru/omegalul/myapplication/MainActivity.kt
+++ app/src/main/java/ru/omegalul/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.omegalul.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.omegalul.omegalultoastlibrary.c9Toast
 import ru.omegalul.omegalultoastlibrary.omegalulToast
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        applicationContext.omegalulToast()   }
+        applicationContext.omegalulToast()
+        applicationContext.c9Toast()
+    }
 }
