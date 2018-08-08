9abcb9e9c29463eb4de19b194a05c75c1a8ab666
dmitriy.smolyaninov <dmitriy.smolyaninov@doconcall.ru>
2018-08-08 17:38:43 +0300

    Add library dependency

--- app/src/main/java/ru/omegalul/myapplication/MainActivity.kt
+++ app/src/main/java/ru/omegalul/myapplication/MainActivity.kt
@@ -2,11 +2,12 @@ package ru.omegalul.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.omegalul.omegalultoastlibrary.omegalulToast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+        applicationContext.omegalulToast()   }
 }
